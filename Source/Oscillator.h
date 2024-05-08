ab3bf886c81da7b141cb249bd9f65f3b0c30c335
jesusherrera94 <jesusherrera1594@gmail.com>
2024-05-07 21:43:12 -0600

    add dc offset limiter.

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -26,6 +26,7 @@ public:
         sin0 = 0.0f;
         sin1 = 0.0f;
         dsin = 0.0f;
+        dc = 0.0f;
     }
     // using additive synthesis method
 //    float nextBandLimitedSample() {
@@ -53,6 +54,7 @@ public:
         if (phase <= PI_OVER_4) {
             float halfPeriod = period / 2.0f;
             phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+            dc = 0.5f * amplitude / phaseMax; // calculate dc offset to remove pop and 0Hz adding!
             phaseMax *= PI;
             inc = phaseMax / halfPeriod;
             phase = -phase;
@@ -75,7 +77,7 @@ public:
             sin0 = sinp;
             output = sinp / phase;
         }
-        return output;
+        return output - dc;
         
     }
 private:
@@ -85,5 +87,6 @@ private:
     float sin0;
     float sin1;
     float dsin;
+    float dc;
 };
 
