9ec439e371b4d99c9e4c0a3130d1fc7f6f60da84
jesusherrera94 <jesusherrera1594@gmail.com>
2024-06-25 21:20:37 -0600

    add personalized guide to rotary and styles to button

--- Source/RotaryKnob.cpp
+++ Source/RotaryKnob.cpp
@@ -18,6 +18,8 @@ RotaryKnob::RotaryKnob()
 {
     slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 100, textBoxHeight);
+    slider.setRotaryParameters(juce::degreesToRadians(225.0f),
+                               juce::degreesToRadians(495.0f), true);
     addAndMakeVisible(slider);
     setBounds(0, 0, 100, 120);
 
